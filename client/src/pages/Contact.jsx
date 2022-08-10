3d6fdade69b5ed2fb390c26990b82910a8e47348
Rotem <rotem.barsela95@gmail.com>
2022-08-11 02:23:59 +0300

    minor fixes

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
                 sectionTitle={"Keep in touch with me 😉"}
             />
             <form ref={form} onSubmit={sendEmail}>
-                <div className='flex flex-row justify-center items-center text-white w-full p-3 mt-10 md:mt-0 md:h-[calc(100vh-256px)]'>
+                <div className='flex flex-row justify-center items-center text-white w-full p-3 mt-5 md:mt-0 md:h-[calc(100vh-256px)]'>
                     <div className='flex flex-col gap-3 w-[90%] md:w-1/2'>
                         {success &&
                             <div className='bg-green-500 border rounded-lg flex justify-center items-center gap-3'>
@@ -55,7 +55,7 @@ const Contact = () => {
                         <InputField type="text" name="user_name" label="Name" />
                         <InputField type="email" name="user_email" label="Email" />
                         <TextAreaField name="message" label="Message" />
-                        <button className='bg-primary uppercase border-2 p-2 border-navbg hover:border-indigo-200' type="submit">Send</button>
+                        <button className='bg-primary uppercase p-2 rounded-md transition-all duration-200 ease-linear hover:bg-indigo-800 shadow-md' type="submit">Send</button>
                     </div>
                 </div>
             </form >
